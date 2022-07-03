cf17e102c7ade3ac17adbe1b0f617464df43c6b0
quest82 <100534170+quest82@users.noreply.github.com>
2022-07-03 15:42:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const firstName = 'Samuel';
+const height = '185.43cm';
+const country = 'Nigeria';
+
+alert('Hello, my name is ' + firstName + ' nice to meet you');
+alert('I am ' + height + ' tall');
+alert('I\'m from ' + country)
